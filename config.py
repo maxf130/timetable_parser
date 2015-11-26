bcce228c2da29e229b1c0e95d016d8e32474e1fb
Maximilian Friedersdorff <maxf130@gmail.com>
2015-11-26 12:58:52 +0000

    Remove semicolons (UGH) from config file
    Switching between C++ and python repeatedly
    leads to these kinds of problems.

--- config.py
+++ config.py
@@ -20,14 +20,14 @@
 # files used by the django framework.  It takes the
 # form of key: value pairs as top level members
 # of this file. 
-TITLE_ROW = 1;
-MODULE_CODE_ROW = 0;
-DAY_ROW = 2;
-START_ROW = 3;
-END_ROW = 4;
-KIND_ROW = 5;
-WEEKS_ROW = 8;
-LECTURER_ROW = 9;
-LOCATION_ROW = 10;
+TITLE_ROW = 1
+MODULE_CODE_ROW = 0
+DAY_ROW = 2
+START_ROW = 3
+END_ROW = 4
+KIND_ROW = 5
+WEEKS_ROW = 8
+LECTURER_ROW = 9
+LOCATION_ROW = 10
 
 MODULES = ['CO7105', 'CO7206', 'CO7210', 'CO7219']
